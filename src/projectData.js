f0c774b45d2e954845c1777893021c735352afe1
Manishkumar76 <mkshakya794@gmail.com>
2024-09-01 22:55:10 +0530

    Add new project

--- src/projectData.js
+++ src/projectData.js
@@ -1,69 +1,73 @@
-import  EventMangement from "./assets/Project/Event-Mangement.jpg";
+import EventMangement from "./assets/Project/Event-Mangement.jpg";
 import Library from "./assets/Project/library.png";
 import Facebook from "./assets/Project/Facebook-clone.png";
 import Calculator from "./assets/Project/calculator.png";
 import clock from "./assets/Project/clock.png";
 import flipkart from "./assets/Project/flipkart.png";
+import NearByMart from "./assets/Project/nearby-mart.png";
 // import { HiLibrary } from "react-icons/hi";
 
-
-
 const projectData = [
-    {
-        name:"Event Management",
-        image:EventMangement,
-        description:"Event Management is a Full Stack Android & Ios Application created using Flutter , Dart , JS , NodeJS and MySQL .",
-        demoLink : "https://github.com/Manishkumar76/project",
-        githubLink : "https://github.com/Manishkumar76/project"
-    },
-{ name: "Nearby Mart ",
-image:,
-description:"Manish Tripathi, Aaryan Verma and me build a Web Application using Laravel that will work as a bridge between the local store owners and customers.",
-demoLink:"",
-githubLink:""
+  {
+    name: "Event Management",
+    image: EventMangement,
+    description:
+      "Event Management is a Full Stack Android & Ios Application created using Flutter , Dart , JS , NodeJS and MySQL .",
+    demoLink: "https://github.com/Manishkumar76/project",
+    githubLink: "https://github.com/Manishkumar76/project"
+  },
+  {
+    name: "Nearby Mart ",
+    image: NearByMart,
+    description:
+      "Manish Tripathi, Aaryan Verma and me build a Web Application using Laravel that will work as a bridge between the local store owners and customers.",
+    demoLink:
+      "https://github.com/Manishkumar76/Nearby-Mart/blob/main/README.md",
+    githubLink: "https://github.com/Manishkumar76/Nearby-Mart"
+  },
+  {
+    name: "Facebook Clone",
+    image: Facebook,
+    description:
+      "In this practice project I have used Flutter, Dart, fetching data from api and more.  ",
+    demoLink: "https://blogs-website-1u2c.vercel.app/",
+    githubLink: "https://github.com/Manishkumar76/Facebook-Flutter-App"
+  },
 
+  {
+    name: "Calculator",
+    image: Calculator,
+    description:
+      "In this practice project I have used HTML, CSS & Javascript. it is responsive and working calculator.",
+    demoLink: "https://manishkumar76.github.io/Calculator_HTML_CSS_JS/",
+    githubLink: "https://github.com/Manishkumar76/Calculator_HTML_CSS_JS"
+  },
 
-    {
-        name:"Facebook Clone",
-        image:Facebook,
-        description:"In this practice project I have used Flutter, Dart, fetching data from api and more.  ",
-        demoLink : "https://blogs-website-1u2c.vercel.app/",
-        githubLink : "https://github.com/Manishkumar76/Facebook-Flutter-App"
-    },
+  {
+    name: "MRSPTU Library Management",
+    image: Library,
+    description:
+      "In this project I have used HTML and CSS and also add different pages of the website.",
+    demoLink: "https://react-router-project-hazel.vercel.app/",
+    githubLink: "https://github.com/laksh0786/React-Router-Project"
+  },
 
+  {
+    name: "Flipkart Frontend Website",
+    image: flipkart,
+    description:
+      "In this project I have used different Html elements and CSS properties.",
+    demoLink: "https://manishkumar76.github.io/flipkart_clone/",
+    githubLink: "https://github.com/Manishkumar76/flipkart_clone"
+  },
 
-    {
-        name:"Calculator",
-        image:Calculator,
-        description:"In this practice project I have used HTML, CSS & Javascript. it is responsive and working calculator.",
-        demoLink : "https://manishkumar76.github.io/Calculator_HTML_CSS_JS/",
-        githubLink : "https://github.com/Manishkumar76/Calculator_HTML_CSS_JS"
-    },
-
-    
-    {
-        name:"MRSPTU Library Management",
-        image:Library,
-        description:"In this project I have used HTML and CSS and also add different pages of the website.",
-        demoLink : "https://react-router-project-hazel.vercel.app/",
-        githubLink : "https://github.com/laksh0786/React-Router-Project"
-    },
-
-    {
-        name:"Flipkart Frontend Website",
-        image:flipkart,
-        description:"In this project I have used different Html elements and CSS properties.",
-        demoLink : "https://manishkumar76.github.io/flipkart_clone/", 
-        githubLink : "https://github.com/Manishkumar76/flipkart_clone"
-    },
-    
-    {
-        name:"Wall-Clock",
-        image:clock,
-        description:"In this project I have used HTML, CSS and Javascripts .",
-        demoLink : "https://manishkumar76.github.io/clock_project/",
-        githubLink : "https://github.com/Manishkumar76/clock_project"
-    },
-]
+  {
+    name: "Wall-Clock",
+    image: clock,
+    description: "In this project I have used HTML, CSS and Javascripts .",
+    demoLink: "https://manishkumar76.github.io/clock_project/",
+    githubLink: "https://github.com/Manishkumar76/clock_project"
+  }
+];
 
 export default projectData;
