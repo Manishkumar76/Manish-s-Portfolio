ba8e5923bb0483e9564b499950375ed67770cbaf
Manishkumar76 <mkshakya794@gmail.com>
2024-12-12 20:34:30 +0530

    profile image update

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { HiArrowNarrowRight } from 'react-icons/hi';
 // import hero from '../assets/home-page.png';
-import manish from '../assets/profile-image2.png';
+import manish from '../assets/profile-image3.png';
 import Typewriter from "typewriter-effect";
 import Introduction from '../components/Introduction';
 import Particle from '../components/Particle';
